ed1785c8084883da31005a9e0940613c5eb39741
zvxzvz <47907111+zvxzvz@users.noreply.github.com>
2019-03-02 06:49:21 +0700

    Update config.js

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,32 +1,32 @@
 const colors = require("../../src/styles/colors");
 
 module.exports = {
-  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
-  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
-  siteDescription: "PersonalBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  siteTitle: "Peace and Victory .ID - A Thread", // <title>
+  shortSiteTitle: "Website", // <title> ending for posts and pages
+  siteDescription: "Mozaik",
+  siteUrl: "https://peaceandvictory.id",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Amin F",
+  authorTwitterAccount: "pv.id",
   // info
-  infoTitle: "greg lobinski",
-  infoTitleNote: "personal blog",
+  infoTitle: "Peace and Victory .ID ",
+  infoTitleNote: "A Thread",
   // manifest.json
-  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
+  manifestName: "Peace and Victory .ID - A Thread",
   manifestShortName: "PersonalBlog", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.background,
   manifestThemeColor: colors.background,
   manifestDisplay: "standalone",
   // contact
-  contactEmail: "john@doe.com",
+  contactEmail: "mr.relawan@gmail.com",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "https://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/zvxzvz" },
+    { name: "twitter", url: "https://twitter.com/zvxzvz" },
+    { name: "facebook", url: "https://facebook.com/zvxzvz" }
   ]
 };
